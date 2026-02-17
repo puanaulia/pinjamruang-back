dde0a18441395521dc1de31a85f3dec09b51424a
Puan <puanauliaraya@gmail.com>
2026-02-17 13:12:45 +0700

    Fix CORS policy to allow frontend access

--- PeminjamRuangAPI/Program.cs
+++ PeminjamRuangAPI/Program.cs
@@ -10,6 +10,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=peminjaman.db"));
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")  // URL frontend
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,13 +31,15 @@ using (var scope = app.Services.CreateScope())
     dbContext.Database.EnsureCreated();
 }
 
+app.UseCors("AllowFrontend");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
